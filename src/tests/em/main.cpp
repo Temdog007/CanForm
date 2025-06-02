4de27fd0dbf27f6a58b3dcae5bf935d42496252e
Temdog007 <temdog007@yahoo.com>
2025-06-02 11:42:29 -0700

    Make tab content scroll

--- src/tests/em/main.cpp
+++ src/tests/em/main.cpp
@@ -105,6 +105,18 @@ bool OnMenuButton(int, const EmscriptenMouseEvent *, void *)
             menu.add("Close", []() { return true; });
             return makeNewMenu("New Menu", std::move(menuList));
         });
+        menu.add("Long Menu", []() {
+            MenuList menuList;
+            auto &menu = menuList.menus.emplace_back();
+            menu.title = "Long Menu";
+            for (size_t i = 0; i < 100; ++i)
+            {
+                char buffer[1024];
+                std::snprintf(buffer, sizeof(buffer), "#%zu", i + 1);
+                menu.add(buffer, []() { return true; });
+            }
+            return makeNewMenu("Long Menu", std::move(menuList));
+        });
     }
     {
         auto &menu = menuList.menus.emplace_back();
